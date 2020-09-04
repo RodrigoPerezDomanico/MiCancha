05905f217f7f39ce3536d532fd2ce843fd135c71
Rodrigo Perez Domanico <rodrigoperez33@hotmail.com>
2020-09-04 17:01:09 -0300

    Updates

---
+++ precache-manifest.ccc49a7f4e4508e9e06feadafa6224d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe0e48c13c10fba4265447e3d6368c0",
+    "url": "/MiCancha/index.html"
+  },
+  {
+    "revision": "9553a16f18fbc69da689",
+    "url": "/MiCancha/static/css/main.58629ff5.chunk.css"
+  },
+  {
+    "revision": "0d2f2b2e0669eef54924",
+    "url": "/MiCancha/static/js/2.62894f7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MiCancha/static/js/2.62894f7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9553a16f18fbc69da689",
+    "url": "/MiCancha/static/js/main.e46c920c.chunk.js"
+  },
+  {
+    "revision": "786f153cf43420b74904",
+    "url": "/MiCancha/static/js/runtime-main.a84e8a4c.js"
+  }
+]);
